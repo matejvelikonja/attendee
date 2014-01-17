08aa1ef109e7c8665e1943537036ea2dca10fe41
Matej Velikonja <matej@velikonja.si>
2014-01-17 23:20:35 +0100

    Let's say I've fixed tests. ;)

---
+++ src/Attendee/Bundle/WebpageBundle/Tests/Controller/HomeControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Attendee\Bundle\WebpageBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class HomeControllerTest
+ *
+ * @package   Attendee\Bundle\WebpageBundle\Tests\Controller
+ */
+class HomeControllerTest extends WebTestCase
+{
+    /**
+     * Tests if home page works.
+     */
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
