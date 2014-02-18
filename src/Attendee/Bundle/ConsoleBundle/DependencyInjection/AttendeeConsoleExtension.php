997849df94e9236661c9cbf23782aad9dc9a24c4
Matej Velikonja <matej@velikonja.si>
2014-02-18 20:39:39 +0100

    unused use statement

--- src/Attendee/Bundle/ConsoleBundle/DependencyInjection/AttendeeConsoleExtension.php
+++ src/Attendee/Bundle/ConsoleBundle/DependencyInjection/AttendeeConsoleExtension.php
@@ -4,7 +4,6 @@ namespace Attendee\Bundle\ConsoleBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
